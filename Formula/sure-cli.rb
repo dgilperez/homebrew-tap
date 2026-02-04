e97641f5fa997666bc67594e4f03b8eba329e459
David Gil - Klaus <dgilperez@gmail.com>
2026-02-04 21:52:03 +0100

    fix: build from root (main.go at root, not cmd/)

--- Formula/sure-cli.rb
+++ Formula/sure-cli.rb
@@ -8,7 +8,7 @@ class SureCli < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/sure-cli"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "."
   end
 
   test do
