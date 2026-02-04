aa95d764af54a4d4f11c4c3be20acf97484f69ea
David Gil - Klaus <dgilperez@gmail.com>
2026-02-04 21:53:51 +0100

    feat: add caveats with PATH instructions

--- Formula/sure-cli.rb
+++ Formula/sure-cli.rb
@@ -11,6 +11,19 @@ class SureCli < Formula
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "."
   end
 
+  def caveats
+    <<~EOS
+      To use sure-cli, ensure Homebrew's bin is in your PATH:
+
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+
+      Add this line to ~/.zshrc or ~/.bash_profile for persistence.
+
+      Then run:
+        sure-cli --help
+    EOS
+  end
+
   test do
     assert_match version.to_s, shell_output("#{bin}/sure-cli version")
   end
